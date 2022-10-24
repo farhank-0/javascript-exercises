0691a192657f23b3a4057fb07b3619f5ba957ac4
farhank-0 <farhankassam0@gmail.com>
2022-10-24 16:00:56 -0400

    tempConversion exercise Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+calc = (x-32) * (5/9);
+return parseFloat(calc.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+calc = (x * 9/5) + 32;
+return parseFloat(calc.toFixed(1));
 };
 
 // Do not edit below this line
