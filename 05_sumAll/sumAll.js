3b25c19b01ff40bba022743e87220ef9a335b829
farhank-0 <farhankassam0@gmail.com>
2022-10-24 15:24:46 -0400

    sumAll exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
-};
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR'
+    } else if (a < b) {
+        for (i = a - 1; i< b + 1; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (a > b) {
+        for (i = b - 1; i< a + 1; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
+;
 
 // Do not edit below this line
 module.exports = sumAll;
