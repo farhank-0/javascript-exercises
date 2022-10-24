8dee102762bf3eb59a8b073ef2f082a627a23cef
farhank-0 <farhankassam0@gmail.com>
2022-10-24 14:15:50 -0400

    reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let stringArr = [];
+    let string = '';
+    let j = 0;
+    for (i = str.length-1; i > -1; i--) {
+        stringArr[j] = str[i];
+        j++;
+    }
+    for (i = 0; i < stringArr.length; i++) {
+        string += stringArr[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
