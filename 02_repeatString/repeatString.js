5eeb1e0e1cd11820309241cc8f808b442ef71d68
farhank-0 <farhankassam0@gmail.com>
2022-10-24 12:48:48 -0400

    Repeat String Exercise
    Completed repeatString() exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(ogstr, repeats) {
+    let string = '';
+if (repeats > 0) {
+    for(i = 0; i < repeats; i++) {
+        string = string + ogstr;
+    }
+} else if (repeats === 0) {
+    return string
+} else {
+    return 'ERROR';
+}
+if (ogstr === string) {
+    return ogstr;
+} else {
+    return string;
+}
 };
 
 // Do not edit below this line
